bb2ba68bfa9f667d32531f1327690c701f0936a2
diegomerola <diego_merola@hotmail.com>
2022-10-31 19:36:00 -0300

    Incorporando funcion asincrnica. Mostrando productos dsp de 2 segundos

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,15 +1,29 @@
 import React from "react";
 import Item from "../Item/Item";
+import { useState, useEffect } from "react";
 import data from "../../utilities/db.js";
+import myPromise from "../../utilities/myPromise";
 
 const ItemList = () => {
+  // UseState para guardar datos de consulta asincronica:
+  const [datos, setDatos] = useState([]);
+
+  // UseEffect para cuando el componente este montado:
+  useEffect(() => {
+    // Hacer consulta asincronica:
+    myPromise(2000, data)
+      .then((response) => setDatos(response))
+      .catch((error) => console.log(error))
+      .finally(() => console.log("Consulta finalizada"));
+  }, []);
+
   return (
     <section>
       <div className="container py-4">
         <div className="row">
           <div className="container pb-5">
             <div className="row justify-content-center">
-              {data.map((element) => (
+              {datos.map((element) => (
                 <Item
                   key={element.id}
                   category={element.category}
